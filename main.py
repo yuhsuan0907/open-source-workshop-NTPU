4367874b002395acb73d7985f9840dd750451401
tzuchennn <tzuuu091818@gmail.com>
2024-05-13 19:48:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,7 @@ from linebot.v3.messaging import (
 )
 import json
 import os
-import requests
-from PIL import Image
-from io import BytesIO
-from firebase import firebase
-import google.generativeai as genai
+
 
 
 # 使用環境變量讀取憑證
